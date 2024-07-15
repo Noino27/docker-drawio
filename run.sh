4df2550539e07952d40b7e67aa2724f83ddae33d
Noino27 <118913473+Noino27@users.noreply.github.com>
2024-07-15 16:20:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@
   sudo tee /"/etc/apt/sources.list.d/docker.list"/ > /"/dev/null"/ \
 && apt-get update \
 && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-&& docker run -i --rm --name='draw' -p 8443:8443 jgraph/drawio"
+&& docker run -i --rm --name="draw" -p 8443:8443 jgraph/drawio"
