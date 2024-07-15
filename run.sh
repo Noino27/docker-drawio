1db789963d1835b0dc4eeb4538a5ab3a2c64e3ee
Noino27 <118913473+Noino27@users.noreply.github.com>
2024-07-15 15:57:31 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-ssh -i /var/lib/jenkins/id_rsa root@10.0.2.104
-ifconfig
+'ssh -i /var/lib/jenkins/id_rsa root@10.0.2.104' \
+'ifconfig' \
 # Add Docker's official GPG key:
-echo Admin123# | sudo -S apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+'sudo apt-get update' \
+'apt-get install ca-certificates curl'\
+'install -m 0755 -d /etc/apt/keyrings' \
+'curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc' \
+'chmod a+r /etc/apt/keyrings/docker.asc' \
 
 # Add the repository to Apt sources:
 echo \
