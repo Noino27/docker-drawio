2ab8c9a24dadcf489a07e03ced75db442fd592ed
Noino27 <118913473+Noino27@users.noreply.github.com>
2024-07-15 16:10:19 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@
   \ deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# Add the repository to Apt sources:
+
 && apt-get update \
 && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-&& docker run -i --rm --name="draw" -p 8443:8443 jgraph/drawio"
+&& docker run -i --rm --name="draw" -p 8443:8443 jgraph/drawio \
+&& sleep 10"
